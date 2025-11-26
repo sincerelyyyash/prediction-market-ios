627bdb382c341e6a2ec7a2806e54aae3db59cdca
sincerelyyyash <yta0088@gmail.com>
2025-11-26 19:13:48 +0530

    chore: initial project configuration and setup

---
+++ Pulse/PulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseApp.swift
+//  Pulse
+//
+//  Created by Yash Thakur on 26/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
